45d4480861a86f04cb28340fa4d7da51468f50cc
Brearey <brearey4@gmail.com>
2021-06-04 10:48:14 +0900

    Added questions from Malysheva

--- youtubeLesson.py
+++ youtubeLesson.py
@@ -3,22 +3,31 @@ from tkinter import messagebox
 
 # Вопросы и ответы
 questionAll = [
-    'Сколько бит памяти компьютера займет слово «информатика»?',
-    'Информацию, не зависящую от чужого мнения, называют:',
-    'Какое устройство ПК предназначено для ввода информации?',
-    'Последний вопрос: как переводиться слово VOID?'
+    'Имеется кортеж вида T = (4, 2, 3). Какая из операций приведёт к тому,\nчто имя T будет ссылаться на кортеж (1, 2, 3)?',
+    'Необходимо собрать и вывести все уникальные слова из строки рекламного текста.\nКакой из перечисленных типов данных Python подходит лучше всего?',
+    'Как вывести список методов и атрибутов объекта x?',
+    'Какая из перечисленных инструкций выполнится быстрее всего, если n = 10**6?',
+    'Вопрос со звёздочкой. Что выведет на экран следующий код?\na, *b, c = [1, 2]\nprint(a, b, c)',
+    'С помощью Python нужно записать данные в файл, но только в том случае, если файла ещё нет.\nКакой режим указать в инструкции open()?',
+    'При объявлении класса с помощью оператора class что пишется\nв круглых скобках после имени класса?'
 ]
 answerTextAll = [
-    ['11 бит', '80 бит', '8 бит', '88 бит'],
-    ['достоверной', 'актуальной', 'объективной', 'полезной'],
-    ['Процессор', 'Монитор', 'Клавиатура', 'Принтер'],
-    ['космос', 'пустота', 'функция', 'уклон']
+    ['T[0] = 1', 'T = (1) + T[1:]', 'T = (1,) + T[1:]', 'T.startswith(1)'],
+    [ 'множество (set)', 'список (list)', 'кортеж (tuple)', 'словарь (dict)'],
+    ['help(x)', 'info(x)', '?x', 'dir(x)'],
+    ['a = list(i for i in range(n))', 'a = [i for i in range(n)]', 'a = (i for i in range(n))', 'a = {i for i in range(n)}'],
+    ['[1] [] [2]', 'Будет вызвано исключение', '1 0 2', "1 [] 2"],
+    ['x', 'Нужна предварительная проверка os.path.exists()', 'w', 'r'],
+    ['Имена аргументов, принимаемых методом __init__', 'Имена принимаемых классом аргументов', 'Имена суперклассов, если класс наследуется\nот одного или нескольких классов', 'Имена классов, порождаемых данным классом']
 ]
 answerAll = [
+    3,
+    1,
     4,
     3,
-    3,
-    2
+    4,
+    1,
+    3
 ]
 
 root = Tk()
@@ -49,19 +58,19 @@ def generateQuestion(root, questionNumber):
     def btn4Clicked():
         checkAnswer(questionNumber, 4)
 
-    btn0 = Button(root, text=answerTextAll[questionNumber][0], font=("Arial Bold", 14), bg="white", width=15)
+    btn0 = Button(root, text=answerTextAll[questionNumber][0], font=("Arial Bold", 14), bg="white", width=40)
     btn0.grid(row=2, column=2, padx=10, pady=10)
     btn0.config(command=btn1Clicked)
 
-    btn1 = Button(root, text=answerTextAll[questionNumber][1], font=("Arial Bold", 14), bg="white", width=15)
+    btn1 = Button(root, text=answerTextAll[questionNumber][1], font=("Arial Bold", 14), bg="white", width=40)
     btn1.grid(row=3, column=2, padx=10, pady=10)
     btn1.config(command=btn2Clicked)
 
-    btn2 = Button(root, text=answerTextAll[questionNumber][2], font=("Arial Bold", 14), bg="white", width=15)
+    btn2 = Button(root, text=answerTextAll[questionNumber][2], font=("Arial Bold", 14), bg="white", width=40)
     btn2.grid(row=4, column=2, padx=10, pady=10)
     btn2.config(command=btn3Clicked)
 
-    btn3 = Button(root, text=answerTextAll[questionNumber][3], font=("Arial Bold", 14), bg="white", width=15)
+    btn3 = Button(root, text=answerTextAll[questionNumber][3], font=("Arial Bold", 14), bg="white", width=40)
     btn3.grid(row=5, column=2, padx=10, pady=10)
     btn3.config(command=btn4Clicked)
 
